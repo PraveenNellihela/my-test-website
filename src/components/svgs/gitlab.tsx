e1f68bacbea97a005e7d51b3e7e88b94cceb01e2
Praveen Nellihela <43164013+PraveenNellihela@users.noreply.github.com>
2025-02-22 22:04:35 +0100

    Create gitlab.tsx

---
+++ src/components/svgs/gitlab.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export default function GitLab({ className }) {
+    return (
+        <svg class="s72 icon-danger">
+            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/assets/icons.svg#issues"></use>
+        </svg>
+    );
+}
